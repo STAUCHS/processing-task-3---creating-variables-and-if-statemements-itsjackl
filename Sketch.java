16ea67a58a4d3c0ef8c1b6f5334c675985e91fc9
itsjackl <jack.lai25@ycdsbk12.ca>
2024-03-06 13:09:22 -0500

    Added random and time

--- Sketch.java
+++ Sketch.java
@@ -16,21 +16,49 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(135, 206, 235);
+
+    // Show Current Time
+    int hr = hour();
+    int min = minute();
+
+    fill(0);
+    textSize(20);
+    text(hr, 10, 30);
+    text(":", 35, 30);
+    text(min, 40, 30);
+
+    float randomX = random(30, 400);
+    float randomY = random(30, 400);
+    // If statements
+    float fltrectcolour1, fltrectcolour2, fltrectcolour3;
+    if (randomX > 200 && randomY > 200){
+      fltrectcolour1 = 196;
+      fltrectcolour2 = 164;
+      fltrectcolour3 = 132;
+    }
+    else if (randomX < 200 && randomY < 200){
+      fltrectcolour1 = 255;
+      fltrectcolour2 = 192;
+      fltrectcolour3 = 203;
+    }
+    else {
+      fltrectcolour1 = 31;
+      fltrectcolour2 = 81;
+      fltrectcolour3 = 255;
+    } 
+    
+    // Main part of the house
+    fill(fltrectcolour1, fltrectcolour2, fltrectcolour3);
+    rect(randomX * 0.25f, randomY * 0.5f, randomX * 0.5f, randomY * 0.5f);
+
+
+    // Roof of the house
+    fill(90, 90, 90);
+    triangle(randomX * 0.25f, randomY * 0.5f, randomX * 0.75f, randomY * 0.5f, randomX * 0.5f, randomY * 0.375f);
+
+    
+    
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
 }
